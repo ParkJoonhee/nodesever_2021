1a80d20a1e1bad93fb11d8c06b59b3b3bcbad69c
PJH <icecrea4@naver.com>
2021-04-21 11:40:14 +0900

    make MyServer.js

---
+++ MyServer.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const hostname = 'localhost';
+const port = 8000;
+const baseUrl = 'http://' + hostname + ':' + 8000;
+
+function start() {
+    function onRequest(req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('Hello World!');
+        res.end();
+    }
+    server = http.createServer(onRequest)
+    server.listen(port, hostname);
+    console.log('Server running at ' + baseUrl);
+}
+
+exports.start = start;
