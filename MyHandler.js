0693d618874073a9c8fd1b51774dfbd78124a54e
PJH <icecrea4@naver.com>
2021-05-12 09:41:36 +0900

    [bug] MyRouter.js:8 => sBody is not defined

--- MyHandler.js
+++ MyHandler.js
@@ -1,5 +1,8 @@
 function start(res) {
-    let body = 'Hello World! <br> I am in the cloud class.';
+    let body = '<head><meta charset = "UTF-8"/></head>'
+    body += '<body><div>Hello World! <br> I am in the cloud class.</div>';
+    body += '<div><a href="\hello">hello 페이지</a></div>'
+    body += '</body>'
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(body);
     res.end();
@@ -12,5 +15,15 @@ function hello(res) {
     res.end();
 }
 
+function wait(res) {
+    setTimeout(function() {
+        let body = 'Thank you for waiting for 5 seconds.';
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(body);
+        res.end();
+    }, 5000);
+}
+
 exports.start = start;
 exports.hello = hello;
+exports.wait = wait;
